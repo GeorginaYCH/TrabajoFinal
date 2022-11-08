df474ca309ad7c6ed969cbb66351605be87efac1
unknown <georgina.karen@gmail.com>
2022-11-08 00:48:33 -0500

    primera version

---
+++ formulario.js
@@ -0,0 +1,10 @@
+function capturar(){
+    let id1= document.getElementById("id1").value;
+    console.log(id1);
+    let id2= document.getElementById("id2").value;
+    console.log(id2);
+    let id3= document.getElementById("id3").value;
+    console.log(id3);
+    let id4= document.getElementById("id4").value;
+    console.log(id4);
+}
